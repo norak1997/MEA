caf930b54a9a969580418d58168d4a57993aa928
Sriman97 <sriman9708@gmail.com>
2018-08-31 08:15:10 +0530

    upto placement

--- MEA/urls.py
+++ MEA/urls.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
+    path('placements', views.placements, name='Placements'),
+    path('tests', views.tests, name='Tests'),
+    path('events', views.events, name='Events'),
+    path('projects',views.projects, name='Projects'),
+    path('projects/<name1>',views.specprojects, name='Projects1'),
+    path('events/<name1>',views.specevents, name='Events1'),
 ]
--- MEA/views.py
+++ MEA/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from .models import Resources, Questions, Events, Placement, Projects
 
 # Create your views here.
 
 def index(request):
-	return render(request,'MEA/index.html', {})
+	resources = Resources.objects.all
+	return render(request,'MEA/index.html', {'resources':resources})
+
+def placements(request):
+	return render(request, 'MEA/placements.html',{})
+
+def events(request):
+	ev = Events.objects.all
+	return render(request, 'MEA/events.html',{'events' : ev})
+
+def tests(request):
+	return render(request, 'MEA/tests.html',{})
+
+def projects(request):
+	proj = Projects.objects.all
+	return render(request, 'MEA/projects.html', {'projects' : proj})
+
+def specprojects(request,name1):
+	proj = Projects.objects.filter(name=name1)
+	return render(request, 'MEA/specprojects.html', {'projects' : proj[0]})
+
+def specevents(request,name1):
+	eve = Events.objects.filter(name=name1)
+	return render(request, 'MEA/specevents.html', {'events' : eve[0]})
 
 	
